f2e42128b8129a8d6f8614d3d7b0c4c2500f0ce7
Giulia-M <giuliamaggio16@gmail.com>
2022-02-04 16:01:05 +0100

    submitting and using the form

--- app01/src/app/app.component.ts
+++ app01/src/app/app.component.ts
@@ -9,7 +9,7 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   title = 'app01';
 
-  onSubmit(form: HTMLFormElement) {
+  onSubmit(form: NgForm) {
     console.log(form);
   }
 }
